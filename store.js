a895f0b3ecb8ac848778e4d2b548584e2f74b985
sEjalBisen <sejalbisen12@gmail.com>
2026-05-13 23:04:22 +0530

    Add files via upload

--- store.js
+++ store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./slices/cartSlice";
+import counterSlice from "./counterSlice"
+import Userslice from "./userSlice";
 
-
-const store= configureStore({
-    reducer:{
-        cartReducer: cartSlice.reducer,
-    }
+const store = configureStore({
+   reducer:{
+    counterState : counterSlice.reducer, // counterState : count
+    userState : Userslice.reducer,// Userslice is stored inside the store with the name userState
+   },
 });
 
 export default store;
