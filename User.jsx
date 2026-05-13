2c24a4e4a46dc2dab3fbdf05f549882629ebea28
sEjalBisen <sejalbisen12@gmail.com>
2026-05-13 23:01:06 +0530

    Add files via upload

--- User.jsx
+++ User.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React , { useState,useEffect } from 'react'
+
 
 function User() {
+    const[user,setUser] = useState(null);
+    const [error,setError] = useState(false);
+    const [loading,setLoading] = useState(true);
+
+    useEffect(function(){
+        (async function(){
+            try{
+                setLoading(true);
+                const resp = await fetch("https://jsonplaceholder.typicode.com/users/1");
+                const user = await resp.json();
+                setLoading(false);
+                setUser(user);
+            }catch(err){
+                setError(true)
+                setLoading(false)
+            }
+        })()
+    },[]);
+
+    const heading = <h2>User Examples</h2>;
+
+    if(loading){
+        return<>{heading}
+                <h3>...loading</h3>
+        </>
+    }
+    if(error){
+        return<>{heading}
+                <h3>Error</h3>
+        </>
+    }
+
   return (
-    <h1>User</h1>
+    <>
+        {heading}
+        <h4>Name: {user.name}</h4>
+        <h4>Phone: {user.phone}</h4>
+    </>
   )
 }
 
