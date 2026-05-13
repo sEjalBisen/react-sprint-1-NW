f65ff5e0e8d4d48758f2a1076e8845c62121dd8e
sEjalBisen <sejalbisen12@gmail.com>
2026-05-13 22:55:35 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,12 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from "./redux/store.js"
 
-createRoot(document.getElementById('root')).render(
- 
+ReactDOM.createRoot(
+  document.getElementById('root'))
+  .render(
+ <Provider store={store}>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-)
+        <App />
+    </BrowserRouter>
+ </Provider>
+    
+,
+  )
