c27347a99d11f02aef7925ec5b3a694f68e8c48e
sEjalBisen <sejalbisen12@gmail.com>
2026-05-13 22:55:09 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,25 +1,28 @@
-import NavBar from './components/NavBar'
-import Home from './components/Home'
-import Product from './components/Product'
-import ProductDetails from './components/ProdcutDetails'
-import PageNotFound from './components/PageNotFound'
-import {Route, Routes, Navigate} from "react-router-dom"
 import './App.css'
+import NavBar from './components/NavBar'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import PageNotFound from './pages/PageNotFound'
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails'
+import User from './pages/User'
+import Cart from './pages/Cart'
+import PaginationProvider from './contexts/PaginationContext';
 
 function App() {
-
   return (
-    <>
-      <NavBar></NavBar>
-      <Home></Home>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/product" element={<Product></Product>}></Route>
-        <Route path="/product/:id" element={<ProductDetails></ProductDetails>}></Route>
+    <BrowserRouter>
+      <PaginationProvider>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}> </Route>
+          <Route path="/cart" element={<Cart></Cart>}></Route>
+          <Route path="/product/:id" element={<ProductDetails></ProductDetails>}> </Route>
+        <Route path="/user" element={<User></User>}></Route>
         <Route path="/home" element={<Navigate to="/"></Navigate>}></Route>
-        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>}> </Route>
       </Routes>
-    </>
+    </PaginationProvider>
+    </BrowserRouter>
   )
 }
 
